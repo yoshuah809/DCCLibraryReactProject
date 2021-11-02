8d285185c9911d16afde1d351d7e1d147259f3ab
unknown <yoshuah809@gmail.com>
2021-11-02 15:58:31 -0500

    added TitleBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import './App.css';
+import TitleBar from './TitleBar/TitleBar';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.books = [
+      { title: 'Bible', author: 'God?' },
+      { title: 'Sonetos de la Muerte', author: 'Gabriela Mistral' },
+      { title: 'To Helen', author: 'Edgar Alan Poe' },
+      { title: 'The Old Man and the Sea', author: 'Ernest Hemingway' },
+    ];
+    this.state = { bookNumber: 1 };
   }
 
   render() {
-    return <h1>Our First React component File </h1>;
+    return (
+      <div className='container-fluid'>
+        <TitleBar />
+        <div className='row'>
+          <div className='col-md-4'>
+            {/*Button to move the previous book viewed*/}
+          </div>
+          <div className='col-md-4'>
+            {/*Display Book with cover here*/}
+            <h1>{this.books[this.state.bookNumber].title}</h1>
+            <h4>{this.books[this.state.bookNumber].author}</h4>
+          </div>
+          <div className='col-md-4'>{/*Button to move the next book */}</div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import reactDOM from 'react-dom';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 reactDOM.render(<App />, document.getElementById('root'));
